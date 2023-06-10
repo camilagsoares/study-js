2455078f1c0a852d6c08167ba322cbc28797adad
camilagsoares <camila.soares19@hotmail.com>
2023-06-10 15:16:36 -0300

    initi

---
+++ begginer/index.js
@@ -0,0 +1,19 @@
+// Aritméticos
+
+/**
+ * +
+ * -
+ * /
+ *  ** é potenciação,seria elevado
+ * 
+ * 
+ * Ordem de execução primeiro
+ * () primeiro
+ * **
+ * / %
+ * + - 
+ */
+
+const num1 = 5;
+const num2 = 10;
+console.log(num1 ** num2);

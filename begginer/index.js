bd30097751552ea259cf9899b25d345ceca36ffc
camilagsoares <camila.soares19@hotmail.com>
2023-06-15 16:35:02 -0300

    function

--- begginer/index.js
+++ begginer/index.js
@@ -30,11 +30,22 @@ contador += 50;
 const nomes = ["Luiz", "Maria", "Bia"];
 
 // nomes.push('Joao','Maria') //adiciona elementos no final do array
-nomes.unshift("Ana") //adiciona no começo do array
-nomes.unshift("Mari")
+nomes.unshift("Ana"); //adiciona no começo do array
+nomes.unshift("Mari");
 
-nomes.shift()
+nomes.shift();
+//
+// console.log(typeof nomes);
 
-console.log(typeof nomes);
+// console.log(nomes instanceof Array);
 
-console.log(nomes instanceof Array);
+function saudacao(nome) {
+  return `Bom dia, ${nome}`
+}
+
+// saudacao("Camila");
+
+const variavel = saudacao();
+console.log(variavel)
+
+// const soma = () => {}

78f934c3cd110c7978e19578a2119924b3ab09da
camilagsoares <camila.soares19@hotmail.com>
2023-06-19 18:18:17 -0300

    object

--- begginer/index.js
+++ begginer/index.js
@@ -62,4 +62,37 @@ function soma(x = 1, y = 2) {
 // };
 
 // se for retornar só uma linha,fica assim
-const raiz = n => n ** 0.5;
+const raiz = (n) => n ** 0.5;
+
+const nomesAlunos = ["Camila", "Ana"];
+nomesAlunos.push("Eduardo");
+nomesAlunos[0] = "Maria";
+// console.log(nomesAlunos)
+
+const alunosEscola = {
+  nome: "Camila",
+  idade: 21,
+
+  fala() {
+    console.log(`Oi,eu tenho ${this.idade}`);
+  },
+  incrementaIdade() {
+    this.idade++;
+  },
+};
+console.log(`${this.idade}`)
+alunosEscola.fala();
+alunosEscola.incrementaIdade();
+alunosEscola.fala();
+// console.log(alunosEscola.nome)
+
+function criaNomes(nome, sobrenome, idade) {
+  return {
+    nome,
+    sobrenome,
+    idade,
+  };
+}
+
+const pessoa1 = criaNomes("Camila", "Soares", 23);
+// console.log(pessoa1);

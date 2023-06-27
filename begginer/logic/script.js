ceb8728cce976e1ebe33ca9e409e8fd136b2fd44
camilagsoares <camila.soares19@hotmail.com>
2023-06-27 14:03:37 -0300

    condition

---
+++ begginer/logic/script.js
@@ -0,0 +1,15 @@
+// const hora = 12;
+
+// if (hora < 12) {
+//   console.log("BOM DIA");
+// } else{
+//     console.log('nao')
+// }
+
+const numero = -1;
+
+if (numero <= 5) {
+    console.log('Sim')
+} else if (numero >= 6 && numero <=8) {
+    console.log("entre 6 e 8")
+}
